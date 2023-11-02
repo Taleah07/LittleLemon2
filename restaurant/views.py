b697767c775b73357333b76ef42a95d0548d77ac
Your Name <you@example.com>
2023-11-02 14:23:55 +0200

    secure

--- restaurant/views.py
+++ restaurant/views.py
@@ -4,6 +4,9 @@ from .models import menu , booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework import generics 
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+
 # Create your views here.
 def index(request):
      return render(request, 'index.html', {})
@@ -17,5 +20,6 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
      serializer_class = MenuSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+   permission_classes = [IsAuthenticated]
    queryset = booking.objects.all()
    serializer_class = BookingSerializer
